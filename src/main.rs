fb072e509694cf609beda6402f1ee0b0046f44af
0xca7 <m1etz@protonmail.com>
2022-02-22 19:00:17 +0100

    copying

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
-/// application to analyze a PCAP file
-/// and output a summary and graph of it
-/// 0xca7
+/*
+    net-analyze - quick summary of pcap dumps
+    Copyright (C) 2022  0xca7
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 use std::env;
 use std::fs::File;
